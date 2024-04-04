9f10a204cdc19f9fb8c68e383de9381a4bc1b8f7
berrecelik <berrecelik5@gmail.com>
2024-04-05 01:09:57 +0200

    updates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
 mongoose
@@ -24,6 +25,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 //parse that body
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -54,7 +56,19 @@ app.get("/campgrounds/:id", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/show", { campground });
 });
-
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+//The spread (...) syntax allows an iterable, such as an array or string,
+//to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected
 app.listen(3000, () => {
   console.log("Serving on port 3000");
 });
